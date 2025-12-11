6afd70127d1d88537b404cde38ae3a4990c298a0
Chaid <vida.mkp@obmontessori.edu.ph>
2025-12-11 23:05:38 +0800

    Update club.py

--- club.py
+++ club.py
@@ -5,9 +5,9 @@ clubs = {
     "savanaclaw": "<h3>Savanaclaw Dorm</h3><p>A dorm based on the King of Beasts' spirit of persistence.</p><p>Housewarden: Leona Kingscholar</p><p> Twisted from The Lion King</p>",
     "octavinelle": "<h3>Octavinelle Dorm</h3><p>A dorm based on the Sea Witch's spirit of benevolence.</p><p>Housewarden: Azul Ashengrotto</p><p> Twisted from The Little Mermaid</p>",
     "scarabia": "<h3>Scarabia Dorm</h3><p>A dorm based on the Sorcerer of the Sands' spirit of mindfulness.</p><p>Housewarden: Kalim Al-Asim</p><p> Twisted from Aladdin</p>",
-    "pomefiore": "<h3>Pomefiore Dorm</h3><p>A dorm based on the Fairest Queen's spirit of tenacity.</p><p>Housewarden: Rook Hunt</p><p> Twisted from Snow White</p>",
-    "ignihyde": "<h3>Ignihyde Dorm</h3><p>A dorm based on the King of the Underworld's spirit of diligence.</p> <p>Housewarden: Jade Leech</p><p> Twisted from Hades in Hercules</p>",
-    "diasomnia": "<h3>Diasomnia Dorm</h3><p>A dorm based on the Thorn Fairy's spirit of nobility.</p> <p>Housewarden: Silver</p><p> Twisted from Sleeping Beauty</p>",
+    "pomefiore": "<h3>Pomefiore Dorm</h3><p>A dorm based on the Fairest Queen's spirit of tenacity.</p><p>Housewarden: Vil Schoenheit</p><p> Twisted from Snow White</p>",
+    "ignihyde": "<h3>Ignihyde Dorm</h3><p>A dorm based on the King of the Underworld's spirit of diligence.</p> <p>Housewarden: Idia Shroud</p><p> Twisted from Hades in Hercules</p>",
+    "diasomnia": "<h3>Diasomnia Dorm</h3><p>A dorm based on the Thorn Fairy's spirit of nobility.</p> <p>Housewarden: Maleus Draconia</p><p> Twisted from Sleeping Beauty</p>",
     "ramshackle": "<h3>Ramshackle Dorm</h3><p>Dorm was once abandoned and haunted by ghosts until Yuu and Grim move in.</p> <p>Housewarden: Yuu (the Protagonist)</p><p> From Twisted Wonderland</p>",
 }
 
@@ -26,3 +26,4 @@ def pome(e=None): show("pomefiore")
 def igni(e=None): show("ignihyde")
 def dias(e=None): show("diasomnia")
 def rams(e=None): show("ramshackle")
+
